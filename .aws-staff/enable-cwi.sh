0e7840714c92d6a031db0b33e90a4044fbc020e6
Andy Thomas <awsandy@amazon.co.uk>
2024-07-10 08:29:40 +0100

    up

--- .aws-staff/enable-cwi.sh
+++ .aws-staff/enable-cwi.sh
@@ -2,9 +2,19 @@ export CLUSTER_NAME=eksworkshop-eksctl
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
 AWS_REGION=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/placement/region 2> /dev/null)
-#aws iam attach-role-policy \
-#--role-name my-worker-node-role \
-#--policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy
+##
+## add CloudWatchAgentServerPolicy
+## eksctl-eksworkshop-eksctl-nodegrou-NodeInstanceRole-CkncYZ4tnEac
+## and KarpenterNodeRole-eksworkshop-eksctl
+##
+rn=$aws iam list-roles --query 'Roles' | jq -r '.[] |select(.RoleName | contains("NodeInst")).RoleName')
+aws iam attach-role-policy \
+--role-name $rn \
+--policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy
+rn=$aws iam list-roles --query 'Roles' | jq -r '.[] |select(.RoleName | contains("KarpenterNode")).RoleName')
+aws iam attach-role-policy \
+--role-name $rn \
+--policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy
 #aws eks create-addon --cluster-name eksworkshop-eksctl --addon-name amazon-cloudwatch-observability
 helm repo add aws-observability https://aws-observability.github.io/helm-charts
 helm repo update aws-observability
